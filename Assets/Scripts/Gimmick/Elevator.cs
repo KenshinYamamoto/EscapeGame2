d6d05cc12902884d6f42cc00a6405fc2fd023fb3
山本　堅心 <baseball0903mk6@docomo.ne.jp>
2022-01-13 11:15:02 +0900

    アニメーションの簡単な実装終了
    タヌキのアニメーション等を実装できるとなお良いと思う。

--- Assets/Scripts/Gimmick/Elevator.cs
+++ Assets/Scripts/Gimmick/Elevator.cs
@@ -6,19 +6,30 @@ public class Elevator : MonoBehaviour
 {
     public GameObject leftObj;
     public GameObject rightObj;
+    Animation anim;
+    bool moved = false;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
 
     private void Start()
     {
         bool clearGimmick = SaveManager.instance.GetGimmickFlag(SaveManager.Flag.OpenedElevator);
         if (clearGimmick)
         {
-            Open();
+            Opened();
         }
     }
 
     // カードを持っていれば開く
     public void OnThis()
     {
+        if (moved)
+        {
+            return;
+        }
         bool hasCard = ItemBox.instance.CanUseItem(ItemManager.Item.Card);
         if (hasCard)
         {
@@ -30,7 +41,13 @@ public class Elevator : MonoBehaviour
 
     void Open()
     {
-        // 左右の扉を消す
+        moved = true;
+        anim.Play();
+    }
+
+    void Opened()
+    {
+        moved = true;
         leftObj.SetActive(false);
         rightObj.SetActive(false);
     }
--- Assets/Scripts/Gimmick/TeaServer.cs
+++ Assets/Scripts/Gimmick/TeaServer.cs
@@ -9,19 +9,31 @@ public class TeaServer : MonoBehaviour
     // タヌキがいなければ移動(消える)
 
     public Tanuki tanuki;
+    Animation anim;
+    bool moved = false;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
 
     // 既に動かしていたら消す
     private void Start()
     {
+        // 既にクリアしているなら
         bool clearGimmick = SaveManager.instance.GetGimmickFlag(SaveManager.Flag.MovedTeaServer);
         if (clearGimmick)
         {
-            Move();
+            Moved();
         }
     }
 
     public void OnThis()
     {
+        if (moved)
+        {
+            return;
+        }
         bool movedTanuki = tanuki.moved;
         if (movedTanuki)
         {
@@ -36,7 +48,13 @@ public class TeaServer : MonoBehaviour
 
     void Move()
     {
-        // 消える
-        gameObject.SetActive(false);
+        moved = true;
+        anim.Play();
+    }
+
+    void Moved()
+    {
+        moved = true;
+        transform.localPosition = new Vector3(22f, -102f, 0);
     }
 }
